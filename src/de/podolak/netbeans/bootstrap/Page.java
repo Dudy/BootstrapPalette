ebc5918109d87fd7b9f0d497e98caaca6b877e7c
dudy <dirk.podolak@gmail.com>
2015-06-29 13:50:40 +0200

    added GLP2 license

--- src/de/podolak/netbeans/bootstrap/Page.java
+++ src/de/podolak/netbeans/bootstrap/Page.java
@@ -1,3 +1,21 @@
+/* 
+ * Copyright (C) 2015 Dirk Podolak
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA
+or see <http://www.gnu.org/licenses/>.
+ */
 package de.podolak.netbeans.bootstrap;
 
 import javax.swing.text.BadLocationException;
@@ -6,7 +24,7 @@ import org.openide.text.ActiveEditorDrop;
 
 /**
  *
- * @author podolak
+ * @author Dirk Podolak
  */
 public class Page implements ActiveEditorDrop {
     
